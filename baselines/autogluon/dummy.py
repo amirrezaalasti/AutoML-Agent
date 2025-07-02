d9cb72d9a12fd544ecdf760ed2586a494eeca01a
Lukas Fehring <lukasfehring@gmail.com>
2025-07-02 10:47:06 +0200

    Minro change to dummy

--- baselines/autogluon/dummy.py
+++ baselines/autogluon/dummy.py
@@ -22,7 +22,7 @@ autogluon_train_data = train_data["X"]
 autogluon_train_data["target"] = train_data["y"].astype(str)
 autogluon_test_data = test_data["X"]
 autogluon_test_data["target"] = test_data["y"].astype(str)
-predictor = TabularPredictor(label="target", log_to_file=True, log_file_path="dummy_log").fit(autogluon_train_data, presets="best_quality",  ag_args_fit={'num_gpus': 1})
+predictor = TabularPredictor(label="target", log_to_file=True, log_file_path="dummy_log").fit(autogluon_train_data, presets="best_quality_hpo",  ag_args_fit={'num_gpus': 1}, time_limit=9000)
 
 performance = predictor.evaluate(autogluon_test_data)
 print(performance)
